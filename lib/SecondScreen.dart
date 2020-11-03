73242c8d7ead101859a434d01c61e61572bb7899
AbanobEmad <AbanobEmad54@yahoo.com>
2020-11-03 18:45:12 +0200

    frist_push

---
+++ lib/SecondScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Tab 2',
+          style: TextStyle(fontSize: 35.0),
+        ),
+      ),
+    );
+  }
+}
